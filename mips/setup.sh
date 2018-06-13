876c0a394bc78f67077800974865d802268534dd
chung96vn <cboybkk59@gmail.com>
2018-06-13 23:15:01 +0700

    Create setup.sh

---
+++ mips/setup.sh
@@ -0,0 +1,24 @@
+#!
+#/bin/sh
+# setup environment for arm architecture
+
+sudo apt-get update
+sudo apt-get install git -y
+
+# setup pwndbg
+cd /tmp
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+
+# setup qemu
+sudo apt-get install qemu -y
+sudo apt-get install qemu-user-static -y
+
+# setup arm lib
+sudo apt-get install gcc-mips-linux-gnu -y
+sudo apt-get install gdb-multiarch -y
+
+#  make symbolic links
+sudo ln -s /usr/mips-linux-gnu/lib/libc-2.23.so /lib/libc.so.6
+sudo ln -s /usr/mips-linux-gnu/lib/ld.so.1 /lib/ld.so.1
